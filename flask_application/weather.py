dbd7f8b7384c91dacf20e850883ddbb88f43ca64
unknown <hayhuhin.new@gmail.com>
2024-01-02 23:38:51 +0200

    added small templates and session redis implementation

--- flask_application/weather.py
+++ flask_application/weather.py
@@ -1,33 +1,51 @@
-from flask import Flask, jsonify, request
+import os
+from datetime import timedelta
+
+import redis
+from flask import Flask, render_template_string, request, session, jsonify,render_template,redirect, url_for
+
+from dotenv import load_dotenv
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-@app.route('/items', methods=['POST'])
-def add_item():
-      # Get the item name from the request body
-      item_name = request.json.get('name')
+load_dotenv(dotenv_path=r"C:\Users\hayhuhin\Desktop\weather_web\backend\api_application\.env")
 
-      # Add the item to the database here
-      # ...
 
-      # Delete the cached response to invalidate the cache
-      cache.delete('items')
+#environment variables
+host_uri = os.environ.get("REDIS_HOST_URI")
+port = os.environ.get("REDIS_PORT")
+username = os.environ.get("REDIS_USERNAME")
+password = os.environ.get("REDIS_PASSWORD")
 
+
+#flass app configuration
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+app.config['REDIS_USERNAME'] = username
+app.config['REDIS_PASSWORD'] = password
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url(f'redis://{host_uri}')
+
+
+#TODO need to add class that accepts the ip and returns json data by the location
+@app.route("/main",methods=["GET"])
+def intro_page():
+      user_ip = request.remote_addr
+      if  user_ip in  session:
+            return render_template("main.html",content=user_ip)
+
+
+@app.route('/weather', methods=['GET'])
+def weather_info():
       return jsonify({'message': 'Item added successfully'})
 
-@app.route('/items', methods=['GET'])
-@cache.cached(timeout=60, key_prefix='items')
-def get_items():
-      # Check if the response is already cached
-      cached_response = redis_client.get('items')
-      if cached_response:
-          return jsonify(cached_response)
 
-      # Get the items from the database here
-      items = Items.query.all()
+if __name__ == "__main__":
+     
+      app.secret_key = 'super secret key'
 
-      # Serialize the items to JSON
-      serialized_items = [item.to_dict() for item in items]
+      app.debug = True
+      app.run()
 
-      response = jsonify(serialized_items)
-
-      return response
